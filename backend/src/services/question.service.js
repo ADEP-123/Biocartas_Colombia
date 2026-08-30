0ca0a679929e8e9a08e25ff291dc13af232b0d5d
Andres Elizalde <andep210399@gmail.com>
2026-08-30 14:04:08 -0700

    fix: :bug: mezclar las opciones en cada respuesta para evitar patron de respuesta en indice 0

--- backend/src/services/question.service.js
+++ backend/src/services/question.service.js
@@ -1,14 +1,16 @@
 const questionRepository = require("../repositories/question.repository");
+const shuffleArray = require("../utils/shuffle");
 
 async function getQuestionsBySpecies(speciesId) {
   return questionRepository.findBySpecies(speciesId);
 }
 
-// Nunca se envía correctOptionIndex al cliente: se validará en el
-// servidor cuando se construya el motor de gamificación (Fase 4).
 async function getPublicQuestionsBySpecies(speciesId) {
   const questions = await questionRepository.findBySpecies(speciesId);
-  return questions.map(({ correctOptionIndex, ...rest }) => rest);
+  return questions.map(({ correctOptionIndex, options, ...rest }) => ({
+    ...rest,
+    options: shuffleArray(options),
+  }));
 }
 
 module.exports = { getQuestionsBySpecies, getPublicQuestionsBySpecies };
