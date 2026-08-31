f5953b97163272f5057e7ea9c48a3a822666459c
Andres Elizalde <andep210399@gmail.com>
2026-08-31 16:30:52 -0700

    perf(backend): enable gzip compression for API responses

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const compression = require("compression");
 
 const env = require("./config/env");
 const routes = require("./routes");
@@ -10,6 +11,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 app.use(cors({ origin: env.corsOrigin }));
+app.use(compression());
 app.use(express.json());
 app.use(morgan("dev"));
 
